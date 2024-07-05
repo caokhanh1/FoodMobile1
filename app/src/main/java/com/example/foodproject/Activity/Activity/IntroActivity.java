5962153d5e87757539ebfc0d34843f6d6d1c78fd
phongdang1 <phongdbde160574@fpt.edu.vn>
2024-07-05 19:46:37 +0700

    Dashboard

--- app/src/main/java/com/example/foodproject/Activity/Activity/IntroActivity.java
+++ app/src/main/java/com/example/foodproject/Activity/Activity/IntroActivity.java
@@ -1,16 +1,11 @@
-package com.example.foodproject.Activity;
+package com.example.foodproject.Activity.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.foodproject.R;
 import com.example.foodproject.databinding.ActivityIntroBinding;
 
 public class IntroActivity extends AppCompatActivity {
