d25f04c50f7744408403250041d8c8f9425c4c51
caokhanh1 <daxxua911@gmail.com>
2024-07-10 14:42:00 +0700

    khanh/userprrofile/changePassword/forgot

--- app/src/main/java/com/example/foodproject/Activity/Activity/LoginActivity.java
+++ app/src/main/java/com/example/foodproject/Activity/Activity/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.foodproject.Activity.Activity;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.foodproject.R;
@@ -24,12 +26,14 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText email, pwd;
     private ProgressBar progressBar;
     private FirebaseAuth auth;
     private TextView view;
+    private  TextView forgot;
     private static final String TAG = "LoginActivity";
 
     @SuppressLint("MissingInflatedId")
@@ -42,16 +46,28 @@ public class LoginActivity extends AppCompatActivity {
         email = findViewById(R.id.editTextText);
         pwd = findViewById(R.id.editTextText1);
         auth = FirebaseAuth.getInstance();
+        view = findViewById(R.id.textView3);
+        forgot=findViewById(R.id.forgot);
         progressBar = findViewById(R.id.progressBar);
-        view=findViewById(R.id.textView3);
-view.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(LoginActivity.this,SignupActivity.class );
-        startActivity(intent);
-        finish();
-    }
-});
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+//
         //Login User
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -87,22 +103,29 @@ view.setOnClickListener(new View.OnClickListener() {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
 
-                            Toast.makeText(LoginActivity.this, "You are logged now", Toast.LENGTH_LONG).show();
-                            Intent intent = new Intent(LoginActivity.this,MainActivity.class );
-                            startActivity(intent);
-                            finish();
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            if (firebaseUser.isEmailVerified()) {
+                                Toast.makeText(LoginActivity.this, "You are logged now", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }else{
+                                firebaseUser.sendEmailVerification();
+                                auth.signOut();
+                                showAlertDialog();
+                            }
+
                         } else {
                             try {
                                 throw task.getException();
                             } catch (FirebaseAuthInvalidUserException e) {
                                 email.setError("User dose not exist");
                                 email.requestFocus();
-                            }
-                            catch (FirebaseAuthInvalidCredentialsException e) {
-                                email.setError("Invalid credentials, re-enter");
+                            } catch (FirebaseAuthInvalidCredentialsException e) {
+                                email.setError("Password false");
                                 email.requestFocus();
-                            }catch (Exception e){
-                                Log.e(TAG,e.getMessage());
+                            } catch (Exception e) {
+                                Log.e(TAG, e.getMessage());
                                 Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
                             }
 
@@ -110,5 +133,26 @@ view.setOnClickListener(new View.OnClickListener() {
                         progressBar.setVisibility(View.GONE);
                     }
                 });
+    }
+
+    private void showAlertDialog() {
+        //set up alert builder
+        AlertDialog.Builder builder=new AlertDialog.Builder(LoginActivity.this);
+        builder.setTitle("Email not verified");
+        builder.setMessage("Please verify your email.You cannot login ith email verification");
+        //open email if user click continue
+        builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_APP_EMAIL);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        //create the dialog
+        AlertDialog alertDialog=builder.create();
+        //show alert
+        alertDialog.show();
     }
 }
---
+++ app/src/main/java/com/example/foodproject/Activity/Activity/UserProfileActivity.java
@@ -0,0 +1,63 @@
+package com.example.foodproject.Activity.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.foodproject.R;
+import com.example.foodproject.databinding.ActivityUserProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class UserProfileActivity extends BaseActivity {
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private ActivityUserProfileBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUserProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        if (user != null) {
+            String displayName = user.getDisplayName();
+            if (displayName != null && !displayName.isEmpty()) {
+                binding.name.setText(displayName);
+            } else {
+                binding.name.setText("No Name");
+            }
+        }
+        binding.changepass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserProfileActivity.this, ChangePasswordActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+        binding.logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                Toast.makeText(UserProfileActivity.this, "You are logged out", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(UserProfileActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.backMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
